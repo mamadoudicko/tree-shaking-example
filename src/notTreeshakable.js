3d6736cb5fb2e817bd862f0f11ca8d1a0ac6b228
Mamadou DICKO <mamadoudicko100@gmail.com>
2023-02-20 15:59:13 +0100

    feat(eslint): add import/no-commonjs rule

--- src/notTreeshakable.js
+++ src/notTreeshakable.js
@@ -1,3 +1,6 @@
+// Disabling the 'import/no-commonjs' rule for the following line of code, which uses CommonJS syntax
+// To see the linter error that would be generated without this rule, remove the above line.
+// eslint-disable-next-line import/no-commonjs
 const { add } = require("./operations"); //CommonJS
 
 const a = 14;
